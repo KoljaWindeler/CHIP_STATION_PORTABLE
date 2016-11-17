b16792340950d778f4dc6725f61ebfd2683fb512
KoljaWindeler <Kolja.Windeler@gmail.com>
2016-11-17 23:04:46 +0100

    added speaker scripts

---
+++ scripts/speaker_mute.sh
@@ -0,0 +1,8 @@
+LABEL_FILE=`grep -l pcf8574a /sys/class/gpio/*/*label`
+BASE_FILE=`dirname $LABEL_FILE`/base
+BASE=`cat $BASE_FILE`
+let PIN=$BASE+4
+echo $PIN > /sys/class/gpio/export
+D="gpio"$PIN
+echo "out" > /sys/class/gpio/$D/direction
+echo 0 > /sys/class/gpio/$D/value
